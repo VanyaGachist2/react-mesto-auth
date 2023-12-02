919fb43b7bdd9cff5dba3f03cdf8675407b66cee
Ivan <vany4golos@yandex.ru>
2023-12-02 18:17:01 +0300

    add router menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,29 @@
+import { Route, Routes } from 'react-router';
 import logo from '../images/logo.svg';
+import { Link } from 'react-router-dom';
 
-function Header () {
+function Header ({ mail, exit }) {
   return (
     <header className="header">
         <img className="header__logo" alt="Россия" src={logo} />
-        <div className='header__log'>
-            <p className='header__mail'>tornadoz-2003@mail.ru</p>
-            <p className='header__condition'>Выйти</p>
-          </div>
+        <Routes>
+          <Route path='/' element={
+            <div className='header__log'>
+              <p className='header__mail'>{mail}</p>
+              <p className='header__condition' onClick={exit}>Выйти</p>
+            </div>
+          } />
+          <Route path='/sign-in' element={
+            <div className='header__log'>
+              <Link to="/sign-up" className='header__condition'>Регистрация</Link>
+            </div>
+          } />
+          <Route path='/sign-up' element={
+            <div className='header__log'>
+              <Link to="/sign-in" className='header__condition'>Войти</Link>
+            </div>
+          } />
+        </Routes>
     </header>
   )
 }
