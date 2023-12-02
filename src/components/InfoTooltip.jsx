eaffad26ca4d8ca5ab2d0c75e75c69606c43bb67
Ivan <vany4golos@yandex.ru>
2023-12-02 18:17:13 +0300

    auth popup

---
+++ src/components/InfoTooltip.jsx
@@ -0,0 +1,13 @@
+function InfoTooltip({ isOpen, onClose, logo, name }) {
+  return (
+    <div className={`popup ${isOpen ? 'popup_opened' : ''}`}>
+      <div className="popup__container">
+        <img className="popup__logo" src={logo} alt="запрос!" />
+        <p className="popup__title popup__title_margin">{name}!</p>
+        <button type="button" className="popup__close" onClick={onClose}></button>
+      </div>
+    </div>
+  )
+}
+
+export default InfoTooltip;
